e508baf8d86fdde60ea7200d960046d7d0160af5
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-10-28 11:45:56 +0200

    js scrub - progress line

--- player.js
+++ player.js
@@ -3,6 +3,7 @@ var mute = document.getElementById("volume-btn");
 var volume = document.getElementById("mute-btn");
 var playIcon = document.getElementById("icon-play-pause");
 var progressLine = document.getElementById("progress-in");
+var progress = document.getElementById("progress-out");
 
 // --- Video play ---
 
@@ -27,6 +28,150 @@ function play() {
         let progressWidth = video.currentTime / video.duration * 100;
         progressLine.style.width = progressWidth + '%';
     })
+
+
+    function scrub(e) {
+        const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+        video.currentTime = scrubTime;
+        console.log(e);
+    }
+    
+    let mousedown = false;
+    progress.addEventListener("click", scrub);
+    progress.addEventListener("mousemove", () => {
+        if (mousedown) {
+            scrub();
+        } 
+    });
+    
+    progress.addEventListener("mousedown", () => mousedown = true);
+    progress.addEventListener("mouseup", () => mousedown = false);
+
+
+
+  // --- FF / Rew ---
+
+  function skip(value) {
+    var video = document.getElementById("video");
+    video.currentTime += value;
+}   
+
+
+// --- Audio mute ---
+
+function audioMute() {
+        video.muted = true;
+        mute.style.display = "none";
+        volume.style.display = "flex";
+        volumeSlider.value = "0";
+}
+
+function audioVolume() {
+        video.muted = false;
+        volume.style.display = "none";
+        mute.style.display = "flex";
+        volumeSlider.value = "50";
+}
+
+var volumeSlider = document.getElementById("volumeslider");
+
+function setVolume() {
+    video.volume = volumeSlider.value / 100;
+    if (volumeSlider.value == "0") {
+        mute.style.display = "none";
+        volume.style.display = "flex";
+        video.muted = true;
+    } else {
+        volume.style.display = "none";
+        mute.style.display = "flex";
+        video.muted = false;
+    }
+}
+
+volumeSlider.addEventListener("mousemove", setVolume);
+
+
+// --- Dropdwn settings ---
+
+function dropDown() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    if (dropdownMenu.style.display === "none") {
+        dropdownMenu.style.display = "flex";
+    } else {
+        dropdownMenu.style.display = "none";
+    }
+}
+
+
+function playPoint5() { 
+    video.playbackRate = 0.5;
+  } 
+
+  function playPoint75() { 
+    video.playbackRate = 0.75;
+  } 
+
+  function play1Point5() { 
+    video.playbackRate = 1.5;
+  } 
+
+  function play2Point() { 
+    video.playbackRate = 2.0;
+  } 
+
+  function playNormal() { 
+    video.playbackRate = 1.0;
+  } 
+
+// --- Fullscreen --- 
+
+var fullScreen = document.getElementById("fullscreen");
+var exitFullscreen = document.getElementById("exit-fullscreen");
+
+var element = document.documentElement;
+var tv = document.getElementById("tv");
+var settingsBar = document.getElementById("move-up");
+
+
+fullScreen.addEventListener ("click", () => {
+    if (element.requestFullscreen) {
+        tv.requestFullscreen()
+        fullScreen.style.display = "none";
+        exitFullscreen.style.display = "flex";
+        settingsBar.style.marginTop = "-115px";
+        settingsBar.style.opacity = "50%";
+        settingsBar.style.background = "black";
+    }
+})
+
+exitFullscreen.addEventListener ("click", () => {
+    if (document.exitFullscreen) {
+        document.exitFullscreen()
+        exitFullscreen.style.display = "none";
+        fullScreen.style.display = "flex";
+        settingsBar.style.marginTop = "0px";
+        settingsBar.style.opacity = "100%";
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // ------> time interval function (odbrojava samo sekunde)
@@ -87,140 +232,6 @@ function play() {
 	//     durTimetext.innerHTML = durationMinutes + ":" + durationSecundes;
 
 
-
-
-  // --- FF / Rew ---
-
-function skip(value) {
-    var video = document.getElementById("video");
-    video.currentTime += value;
-}   
-
-
-// --- Audio mute ---
-
-function audioMute() {
-        video.muted = true;
-        mute.style.display = "none";
-        volume.style.display = "flex";
-        volumeSlider.value = "0";
-}
-
-function audioVolume() {
-        video.muted = false;
-        volume.style.display = "none";
-        mute.style.display = "flex";
-        volumeSlider.value = "50";
-}
-
-var volumeSlider = document.getElementById("volumeslider");
-
-function setVolume() {
-    video.volume = volumeSlider.value / 100;
-    if (volumeSlider.value == "0") {
-        mute.style.display = "none";
-        volume.style.display = "flex";
-        video.muted = true;
-    } else {
-        volume.style.display = "none";
-        mute.style.display = "flex";
-        video.muted = false;
-    }
-}
-
-volumeSlider.addEventListener("mousemove", setVolume);
-
-
-// --- Dropdwn settings ---
-
-function dropDown() {
-    var dropdownMenu = document.getElementById("dropdown-menu");
-    if (dropdownMenu.style.display === "none") {
-        dropdownMenu.style.display = "flex";
-    } else {
-        dropdownMenu.style.display = "none";
-    }
-}
-
-
-
-function playPoint5() { 
-    video.playbackRate = 0.5;
-  } 
-
-  function playPoint75() { 
-    video.playbackRate = 0.75;
-  } 
-
-  function play1Point5() { 
-    video.playbackRate = 1.5;
-  } 
-
-  function play2Point() { 
-    video.playbackRate = 2.0;
-  } 
-
-  function playNormal() { 
-    video.playbackRate = 1.0;
-  } 
-
-// --- Fullscreen --- 
-
-var fullScreen = document.getElementById("fullscreen");
-var exitFullscreen = document.getElementById("exit-fullscreen");
-
-var element = document.documentElement;
-var tv = document.getElementById("tv");
-var settingsBar = document.getElementById("move-up");
-
-
-fullScreen.addEventListener ("click", () => {
-    if (element.requestFullscreen) {
-        tv.requestFullscreen()
-        fullScreen.style.display = "none";
-        exitFullscreen.style.display = "flex";
-        settingsBar.style.marginTop = "-115px";
-        settingsBar.style.opacity = "50%";
-        settingsBar.style.background = "black";
-    }
-})
-
-exitFullscreen.addEventListener ("click", () => {
-    if (document.exitFullscreen) {
-        document.exitFullscreen()
-        exitFullscreen.style.display = "none";
-        fullScreen.style.display = "flex";
-        settingsBar.style.marginTop = "0px";
-        settingsBar.style.opacity = "100%";
-    }
-})
-
-
-
-
-
- 
-var seekto;
-var seeking = false;
-
-
-progressLine.addEventListener("mousedown", function(event) { 
-    seeking = true; seek(event); 
-});
-progressLine.addEventListener("mousemove", function(event) { 
-    seek(event); 
-});
-progressLine.addEventListener("mouseup",function() { 
-    seeking = false; 
-});
-
-function seek (event) {
-    if (seeking) {
-        progressLine.value = event.clientX - progressLine.offsetLeft;
-        seekto = video.duration * (progressLine.value / 100);
-        video.currentTime = seekto;
-    }
-}
 
 
 
